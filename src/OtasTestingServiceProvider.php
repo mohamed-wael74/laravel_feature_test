12a59ec0705f0938eca299de8c63d2fe83624a41
omar-ashraf-s <o.ashraf@otas.net>
2025-05-15 11:04:51 +0300

    wip: add service provider

---
+++ src/OtasTestingServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Otas\Testing;
+
+use Illuminate\Support\ServiceProvider;
+
+class OtasTestingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
