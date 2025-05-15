5baf39bbf2642e9bf6469123618825e88a537d92
omar-ashraf-s <o.ashraf@otas.net>
2025-05-15 10:59:57 +0300

    wip: add autoload and tests

---
+++ tests/Unit/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Otas\Testing\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase 
+{
+    public function test_initial()
+    {
+        $this->assertTrue(true);
+    }
+}
